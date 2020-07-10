8737ac3e8daa0718ccf2dab765d3fb57146f2c45
Cherie <cherietsukayama@gmail.com>
2020-07-10 21:07:36 +0000

    revised the layout ofthe home page. Created the homepage to say 'Welcome Home'

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  // Switch,
+  // Route,
+  Link,
+  // useRouteMatch,
+  // useParams
+} from "react-router-dom";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <Router>
+      <div className="container d-flex justify-content-center">
+        <nav className="my-4">
+          <ul className="row">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/">About</Link>
+            </li>
+            <li>
+              <Link to="/">Users</Link>
+            </li>
+          </ul>
+        </nav>
+        {/* <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch> */}
+      </div>
 
-export default App;
+    </Router>
+  )
+}
