91607e5aec78a108c5aee73d56e21f9061e4d9e5
Cherie <cherietsukayama@gmail.com>
2020-07-10 22:40:52 +0000

    added styling to the topics and topic components

--- src/components/topics.jsx
+++ src/components/topics.jsx
@@ -11,24 +11,37 @@ export default function Topics() {
   let match = useRouteMatch();
   console.log('match:', match);
   return (
-    <div>
+    <div className="container">
       <h1 className='text-center col-12'>Topics</h1>
-      <ul className='row justify-content-center'>
-        <li>
-          <Link to={`${match.url}/components`}> Components </Link>
-        </li>
-        <li>
-          <Link to={`${match.url}/props-v-state`}> Props VS State </Link>
-        </li>
-      </ul>
-      <Switch>
-        <Route path={`${match.path}/:topicId`}>
-          <Topic />
-        </ Route>
-        <Route>
-          <h3>Please select a topic.</h3>
-        </Route>
-      </Switch>
+      <div className='row'>
+        <div className="col-3">
+          <div className='col custom-card'>
+            <ul className='p-0 my-4'>
+              <li>
+                <Link to={`${match.url}/components`}> Components </Link>
+              </li>
+              <li>
+                <Link to={`${match.url}/props-v-state`}> Props VS State </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="d-flex justify-content-center col">
+          <div className="col-11 card py-4">
+            <Switch>
+              <Route path={`${match.path}/:topicId`}>
+                <Topic />
+              </ Route>
+              <Route>
+                <h3>Please select a topic.</h3>
+              </Route>
+            </Switch>
+          </div>
+        </div>
+
+      </div>
+
     </div>
   )
 }
